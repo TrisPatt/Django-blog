453e3cf82ccfa16cfdbae991c6c2c4419ac220d2
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-07-28 17:06:11 +0000

    update post model

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
+    exerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
 
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
